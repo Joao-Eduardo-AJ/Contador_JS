48ddb38d0e758b8a766d0239a9772e5783ca080f
João Eduardo Amorim Jardim <joaoeduaj@gmail.com>
2022-07-06 23:01:35 -0300

    Adicionei mais alguns pequenos projetos

---
+++ Testes independentes/palíndromo.js
@@ -0,0 +1,24 @@
+/* Resolução 1 */
+
+/* function verificarPalindromo(string) {
+    if (!string) return;
+
+    return string.split("").reverse().join("") === string;
+}
+
+console.log(verificarPalindromo("renner")); */
+
+/* Resolução 2 */
+
+function verificarPalindromo(string) {
+    if (!string) return "string inexistente";
+
+    for (let i = 0; i < string.length / 2; i++) {
+        if (string[i] !== string[string.length - 1 - i]) {
+        return false;       
+        }
+    }
+    return true;
+}
+
+console.log(verificarPalindromo("abcdedcba"));
---
+++ Testes independentes/substiruirPares.js
@@ -0,0 +1,16 @@
+function substituiPares(array) {
+    for (let i = 0; i < array.length; i < i++) {
+        if (array[i] === 0) {
+            console.log(`${array[i]} Você já é zero!`)
+        } else if (array[i] % 2 === 0) {
+            console.log(`Substituindo ${array[i]} por 0...`)
+            array[i] = 0;
+        } else {
+        }
+    }
+    return array;
+}
+
+let arr = [0, 1, 3, 4, 6, 80, 33, 23, 90];
+
+console.log(substituiPares(arr));
