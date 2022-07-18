ba70b455226f6e2a19d7c58071d9979e334c7af3
João Eduardo Amorim Jardim <joaoeduaj@gmail.com>
2022-07-18 18:26:40 -0300

    Módulos
    Aprendi a usar módulos, e organizei de forma mais efetiva os arquivos deste repositório

---
+++ Módulos/main.mjs
@@ -0,0 +1,5 @@
+import {mostraCidade, mostraIdade, mostraHobby} from "./funcoes.mjs";
+
+console.log(mostraIdade('João', 21));
+console.log(mostraCidade('João', 'Nova Santa Rita'));
+console.log(mostraHobby('João', 'jogar'));
